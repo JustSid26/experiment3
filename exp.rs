5cfe8e7b9f66240d9bb2c9b443d70610f1d20ed8
justSid26 <lamasidharth25@gmail.com>
2026-02-03 07:49:56 +0530

    added app ID

--- exp.rs
+++ exp.rs
@@ -1,4 +1,6 @@
 fn main(){
-    let mut name = String::from("Siddharth Lama")
+    let mut name = String::from("Siddharth Lama");
     println!("Name: {name}");
+    let mut app_id = 2407926;
+    println!("App ID: {app_id}")
 }
